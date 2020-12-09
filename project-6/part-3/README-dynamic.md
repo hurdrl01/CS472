b0f7ccd843a2065da43375c563558303d29c2dc5
hurdrl01 <hurdrl01@pfw.edu>
2020-12-08 19:51:35 -0500

    Updated files, fixed README

--- project-6/part-3/README-dynamic.md
+++ project-6/part-3/README-dynamic.md
@@ -2,12 +2,148 @@
 ## Author(s): Ryan Hurd & Vinamra Munot <br/>
 Professor: Dr. Chen<br/>
 Course: CS 472 - Operating System Design <br/>
-Due Date: December 2, 2020 11:59 PM<br/>
+Due Date: December 9, 2020 11:59 PM<br/>
 
-We used a paired programming method, we programmed on one machine sharing ideas and code until we arrived at a finished product <br>
-## For Part Three:  Multiple threads using producer/consumer, but with unbounded buffer <br/>
-### **** <br/> 
-<br/>
+## For Part Three: Consumer and Producer <br/>
 
- ### **** <br/> 
- <br/>
+Basically consumer comes in and checks if there are any tasks. Producer creates the tasks in the queue.
+The queue data structure:
+- Queue operations:
+    - enqueue
+    - dequeue
+    - init
+    - clean 
+
+```c
+
+/* job description */
+typedef struct job {
+    char *read_buffer;
+    int read_size; 
+    char *result_buffer;
+    int result_size;
+    struct job *next;
+} zip_job;
+
+/* linkied list queue */
+typedef struct queue {
+    zip_job *head;
+    zip_job *tail;
+    zip_job *current; // used by consumers to indicate the current running job or the dummy node 
+    pthread_cond_t fill; // indicate that the queue is not empty
+    int count;   // number of available unrun jobs in queue 
+    pthread_mutex_t lock; 
+    int done;   // indicate that producer is done 
+} zip_queue;
+```
+We have used the above data structures and then used the printOutput to print the result. Also used page_size to be as 4096. 
+
+```c
+void *consumer(){
+	do{
+		pthread_mutex_lock(&lock);
+		while(q_size==0 && isComplete==0){
+		    pthread_cond_signal(&empty);
+			pthread_cond_wait(&fill,&lock); //call the producer to start filling the queue.
+		}
+		if(isComplete==1 && q_size==0){ //If producer is done mapping and there's nothing left in the queue.
+			pthread_mutex_unlock(&lock);
+			return NULL;
+		}
+		struct buffer temp=get();
+		if(isComplete==0){
+		    pthread_cond_signal(&empty);
+		}	
+		pthread_mutex_unlock(&lock);
+		//Output position calculation
+		int position=calculateOutputPosition(temp);
+		out[position]=RLECompress(temp);
+	}while(!(isComplete==1 && q_size==0));
+	return NULL;
+}
+```
+
+```c
+void* producer(void *arg){
+	//Step 1: Get the file.
+	char** filenames = (char **)arg;
+	struct stat sb;
+	char* map; //mmap address
+	int file;
+	
+	//Step 2: Open the file
+	for(int i=0;i<num_files;i++){
+		//printf("filename %s\n",filenames[i]);
+		file = open(filenames[i], O_RDONLY);
+		int pages_in_file=0; //Calculates the number of pages in the file. Number of pages = Size of file / Page size.
+		int last_page_size=0; //Variable required if the file is not page-alligned ie Size of File % Page size !=0
+		
+		if(file == -1){ //yikes , possible due to file not found?
+			printf("Error: File didn't open\n");
+			exit(1);
+		}
+
+		//Step 3: Get the file info.
+		if(fstat(file,&sb) == -1){ //yikes #2.
+			close(file);
+			printf("Error: Couldn't retrieve file stats");
+			exit(1);
+		}
+		//Empty files - Test 5
+        	if(sb.st_size==0){
+               		continue;
+        	}
+ 
+		pages_in_file=(sb.st_size/page_size);
+		if(((double)sb.st_size/page_size)>pages_in_file){ 
+			pages_in_file+=1;
+			last_page_size=sb.st_size%page_size;
+		}
+		else{ //Page alligned
+			last_page_size=page_size;
+		}
+		total_pages+=pages_in_file;
+		pages_per_file[i]=pages_in_file;
+
+		map = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, file, 0);									  
+		if (map == MAP_FAILED) { //yikes #3,possibly due to no memory? --unmap needed then?
+			close(file);
+			printf("Error mmapping the file\n");
+			exit(1);
+    	}	
+    	
+		for(int j=0;j<pages_in_file;j++){
+			pthread_mutex_lock(&lock);
+			while(q_size==q_capacity){
+			    pthread_cond_broadcast(&fill); //Wake-up all the sleeping consumer threads.
+				pthread_cond_wait(&empty,&lock); //Call the consumer to start working on the queue.
+			}
+			pthread_mutex_unlock(&lock);
+			struct buffer temp;
+			if(j==pages_in_file-1){ //Last page, might not be page-alligned
+				temp.last_page_size=last_page_size;
+			}
+			else{
+				temp.last_page_size=page_size;
+			}
+			temp.address=map;
+			temp.page_number=j;
+			temp.file_number=i;
+			map+=page_size; //Go to next page in the memory.
+			//possible race condition while changing size.
+			pthread_mutex_lock(&lock);
+			put(temp);
+			pthread_mutex_unlock(&lock);
+			pthread_cond_signal(&fill);
+		}
+		//Step 7: Close the file.
+		close(file);
+	}
+
+	isComplete=1; 
+	pthread_cond_broadcast(&fill); 
+	return 0;
+}
+```
+
+The above code is what we used for producer and consumer
--- project-6/part-3/pzip-dynamic.c
+++ project-6/part-3/pzip-dynamic.c
@@ -1,3 +1,12 @@
+/*
+Project 6 - Parallel Zip
+Author(s): Ryan Hurd & Vinamra Munot
+Professor: Dr. Chen
+Course: CS 472 - Operating System Design
+Due Date: December 2, 2020 11:59 PM
+*/
+
+
 ////////////////////////////////////////////////////////////////////////
 /*							LIBRARIES								  */
 ////////////////////////////////////////////////////////////////////////
@@ -16,10 +25,10 @@
 ////////////////////////////////////////////////////////////////////////
 /*						GLOBAL VARIABLES							  */
 ////////////////////////////////////////////////////////////////////////
-int total_threads; 							// Total number of threads that will be created for consumer.
-int page_size; 								// Page size = 4096 Bytes
+int num_thread; 							// Total number of threads that will be created for consumer.
+int pg_sz; 									// Page size = 4096 Bytes
 int num_files; 								// Total number of the files passed as the arguments.
-int isComplete=0; 							// Flag to wakeup 
+int isComp=0; 								// Flag to wakeup 
 //Flag needed to wakeup any sleeping threads at the end of program.
 
 int total_pages; 							// To compress output
@@ -29,7 +38,7 @@ int q_tail=0; 								// Queue tail.
 int q_size=0; 								// Queue capacity.
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER, filelock=PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t empty = PTHREAD_COND_INITIALIZER, fill = PTHREAD_COND_INITIALIZER;
-int* pages_per_file;
+int* pagePerFile;
 
 
 
@@ -46,15 +55,13 @@ struct buffer {								// Contains page data specific to a file
     char* address; 							// Address to map, file_number file + page_number page
     int file_number; 						// File Number
     int page_number; 						// Page number
-    int last_page_size; 					// Page sized 
+    int last_pg_sz; 						// Page sized 
 } buf[q_capacity];
 
 struct fd{									// Contains file specific data for munmap
 	char* addr;
 	int size;
 } *files;
-
-
 
 
 
@@ -80,31 +87,31 @@ void freeMemory();
 ////////////////////////////////////////////////////////////////////////
 /*						MAIN FUNCTION								  */
 ////////////////////////////////////////////////////////////////////////
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 	// Check if less than two arguments are given
-	if(argc<2){
+	if(argc<2) {
 		printf("pzip: file1 [file2 ...]\n");
 		exit(1);
 	}
 
 	// Variable creation and declaration
-	page_size = 4096;								// Size of a page
+	pg_sz = 4096;									// Size of a page
 	num_files=argc-1; 								// Number of files, necessary for producer
-	total_threads=get_nprocs(); 					// Number of processes consumer threads 
-	pages_per_file=malloc(sizeof(int)*num_files); 	// Number of pages per file
+	num_thread=get_nprocs(); 						// Number of processes consumer threads 
+	pagePerFile=malloc(sizeof(int)*num_files); 		// Number of pages per file
     out=malloc(sizeof(struct output)* 512000*2); 	// Output variable
 	
 	// Create thread of producer to map files
-	pthread_t pid,cid[total_threads];
+	pthread_t pid,cid[num_thread];
 	pthread_create(&pid, NULL, producer, argv+1); 	// argv[0] is not need, so we skip that by using argv + 1
 
 	// Create thread of consumer to compress all pages in a file
-	for (int i = 0; i < total_threads; i++) {
+	for (int i = 0; i < num_thread; i++) {
         pthread_create(&cid[i], NULL, consumer, NULL);
     }
 
 	// Wait for producers and consumer to finish
-    for (int i = 0; i < total_threads; i++) {
+    for (int i = 0; i < num_thread; i++) {
         pthread_join(cid[i], NULL);
     }
 
@@ -122,9 +129,9 @@ int main(int argc, char* argv[]){
 ////////////////////////////////////////////////////////////////////////
 /*					FUNCTION DEFINITIONS							  */
 ////////////////////////////////////////////////////////////////////////
-//buf is the Buffer queue. Queue capacity by default = 10
-//Add at q_head index of the circular queue. 
-void put(struct buffer b){
+// buf is Buffer queue with capacity of 10
+// Add a q_head index  
+void put(struct buffer b) {
   	buf[q_head] = b; //Enqueue the buffer
   	q_head = (q_head + 1) % q_capacity;
   	q_size++;
@@ -134,123 +141,94 @@ void put(struct buffer b){
 
 /*					Producer Stuff									  */
 // Producer function for memory mapping files that are passed as arguments
-void* producer(void *arg){
-	//Step 1: Get the file.
+void* producer(void *arg) {
 	char** filenames = (char **)arg;
 	struct stat sb;
-	char* map; //mmap address
+	char* map; 										// mmap address
 	int file;
 	
-	//Step 2: Open the file
-	for(int i=0;i<num_files;i++){
-		//printf("filename %s\n",filenames[i]);
+	for(int i=0;i<num_files;i++) {
 		file = open(filenames[i], O_RDONLY);
-		int pages_in_file=0; //Calculates the number of pages in the file. Number of pages = Size of file / Page size.
-		int last_page_size=0; //Variable required if the file is not page-alligned ie Size of File % Page size !=0
+		int pages_in_file=0; 						// Calculate the number of page in the file. Number of pages = Size of file / Page size.
+		int last_pg_sz=0; 							// Variable required if the file is not page-alligned ie Size of File % Page size !=0
 		
-		if(file == -1){ //yikes , possible due to file not found?
+		if(file == -1) {
 			printf("Error: File didn't open\n");
 			exit(1);
 		}
 
-		//Step 3: Get the file info.
-		if(fstat(file,&sb) == -1){ //yikes #2.
+		if(fstat(file,&sb) == -1) { 
 			close(file);
 			printf("Error: Couldn't retrieve file stats");
 			exit(1);
 		}
-		//Empty files - Test 5
-        	if(sb.st_size==0){
+		
+        	if(sb.st_size==0) {
                		continue;
         	}
-		//Step 4: Calculate the number of pages and last page size.
-		//st_size contains the size offset. 
-		pages_in_file=(sb.st_size/page_size);
-		//In case file is not page alligned, we'll need to assign an extra page.
-		if(((double)sb.st_size/page_size)>pages_in_file){ 
+
+		pages_in_file=(sb.st_size/pg_sz);
+
+		if(((double)sb.st_size/pg_sz)>pages_in_file) { 
 			pages_in_file+=1;
-			last_page_size=sb.st_size%page_size;
+			last_pg_sz=sb.st_size%pg_sz;
 		}
-		else{ //Page alligned
-			last_page_size=page_size;
+		else { 
+			last_pg_sz=pg_sz;
 		}
 		total_pages+=pages_in_file;
-		pages_per_file[i]=pages_in_file;
-		//Compressed output pointer. Re-allocates for every new page.
-		/*if(out==NULL){
-			out=malloc(total_pages*sizeof(struct output));
-		}
-		else{
-			struct output* newOut=realloc(out,sizeof(struct output)*total_pages);
-			if(newOut==NULL){
-				printf("Error: Problem with re-allocating\n");
-				exit(1);
-			}
-			else{
-				out=newOut;
-			}
-		}*/
-		//Step 5: Map the entire file.
-		map = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, file, 0); //If addr is NULL, then the kernel chooses the (page-aligned) address
-		//at which to create the mapping. Source: man pages															  
-		if (map == MAP_FAILED) { //yikes #3,possibly due to no memory? --unmap needed then?
+		pagePerFile[i]=pages_in_file;
+
+		map = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, file, 0);														  
+		if (map == MAP_FAILED) { 
 			close(file);
 			printf("Error mmapping the file\n");
 			exit(1);
     	}	
     	
-    	//Needed for munmap
-    	// files[i].addr=map;
-    	// files[i].size=sb.st_size;
-    	//Step 6: For all the pages in file, create a Buffer type data with the relevant information for the consumer.
-		for(int j=0;j<pages_in_file;j++){
+		for(int j=0;j<pages_in_file;j++) {
 			pthread_mutex_lock(&lock);
-			while(q_size==q_capacity){
-			    pthread_cond_broadcast(&fill); //Wake-up all the sleeping consumer threads.
-				pthread_cond_wait(&empty,&lock); //Call the consumer to start working on the queue.
+			while(q_size==q_capacity) {
+			    pthread_cond_broadcast(&fill); 
+				pthread_cond_wait(&empty,&lock);
 			}
 			pthread_mutex_unlock(&lock);
 			struct buffer temp;
-			if(j==pages_in_file-1){ //Last page, might not be page-alligned
-				temp.last_page_size=last_page_size;
+			if(j==pages_in_file-1) {
+				temp.last_pg_sz=last_pg_sz;
 			}
 			else{
-				temp.last_page_size=page_size;
+				temp.last_pg_sz=pg_sz;
 			}
 			temp.address=map;
 			temp.page_number=j;
 			temp.file_number=i;
-			map+=page_size; //Go to next page in the memory.
-			//possible race condition while changing size.
+			map+=pg_sz; 
+
 			pthread_mutex_lock(&lock);
 			put(temp);
 			pthread_mutex_unlock(&lock);
 			pthread_cond_signal(&fill);
 		}
-		//Step 7: Close the file.
 		close(file);
 	}
-	//Possible race condition at isComplete?
-	isComplete=1; //When producer is done mapping.
-	//Debugging step: Program wasn't ending during some runtimes as consumer threads were waiting for work.
-	pthread_cond_broadcast(&fill); //Wake-up all the sleeping consumer threads.
-	//printf("producer exiting with queue_size %d\n",q_size);
+	isComp=1;
+	pthread_cond_broadcast(&fill);
 	return 0;
 }
 
 
 
 /*					Consumer Stuff									  */
-//Compresses the buffer object.
-struct output RLECompress(struct buffer temp){
+struct output RLECompress(struct buffer temp) {
 	struct output compressed;
-	compressed.count=malloc(temp.last_page_size*sizeof(int));
-	char* tempString=malloc(temp.last_page_size);
+	compressed.count=malloc(temp.last_pg_sz*sizeof(int));
+	char* tempString=malloc(temp.last_pg_sz);
 	int countIndex=0;
-	for(int i=0;i<temp.last_page_size;i++){
+	for(int i=0;i<temp.last_pg_sz;i++) {
 		tempString[countIndex]=temp.address[i];
 		compressed.count[countIndex]=1;
-		while(i+1<temp.last_page_size && temp.address[i]==temp.address[i+1]){
+		while(i+1<temp.last_pg_sz && temp.address[i]==temp.address[i+1]) {
 			compressed.count[countIndex]++;
 			i++;
 		}
@@ -263,57 +241,52 @@ struct output RLECompress(struct buffer temp){
 }
 
 
-//Calculates the relative output position for the buffer object.
-int calculateOutputPosition(struct buffer temp){
+
+int calculateOutputPosition(struct buffer temp) {
 	int position=0;
-	// Step 1: Find the relative position of the buffer object
-	//step 1: Find the relative file position of the buffer object.
-	for(int i=0;i<temp.file_number;i++){
-		position+=pages_per_file[i];
+	for(int i=0;i<temp.file_number;i++) {
+		position+=pagePerFile[i];
 	}
-	// Step 2: Find the relative postion of the page
 	position+=temp.page_number;
 	return position;	
 }
 
 
-// Consumer thread to start compression of memory for file in the 'buf' queue
-void *consumer(){
+void *consumer() {
 	do{
 		pthread_mutex_lock(&lock);
-		while(q_size==0 && isComplete==0){
+		while(q_size==0 && isComp==0) {
 		    pthread_cond_signal(&empty);
 			pthread_cond_wait(&fill,&lock); 
 		}
-		if(isComplete==1 && q_size==0){ 
+		if(isComp==1 && q_size==0) { 
 			pthread_mutex_unlock(&lock);
 			return NULL;
 		}
 		struct buffer temp=get();
-		if(isComplete==0){
+		if(isComp==0) {
 		    pthread_cond_signal(&empty);
 		}	
 		pthread_mutex_unlock(&lock);
 		int position=calculateOutputPosition(temp);
 		out[position]=RLECompress(temp);
-	}while(!(isComplete==1 && q_size==0));
+	}while(!(isComp==1 && q_size==0));
 	return NULL;
 }
 
 /*					Main Stuff									  */
-
-void printOutput(){
-	char* finalOutput=malloc(total_pages*page_size*(sizeof(int)+sizeof(char)));
+void printOutput() {
+	char* finalOutput=malloc(total_pages*pg_sz*(sizeof(int)+sizeof(char)));
     char* init=finalOutput; 
-    for(int i=0;i<total_pages;i++){
-		if(i<total_pages-1){
-			if(out[i].data[out[i].size-1]==out[i+1].data[0]){
+    for(int i=0;i<total_pages;i++) {
+		if(i<total_pages-1) {
+			if(out[i].data[out[i].size-1]==out[i+1].data[0]) {
 				out[i+1].count[0]+=out[i].count[out[i].size-1];
 				out[i].size--;
 			}
 		}
 		
-		for(int j=0;j<out[i].size;j++){
+		for(int j=0;j<out[i].size;j++) {
 			int num=out[i].count[j];
 			char character=out[i].data[j];
 			*((int*)finalOutput)=num;
@@ -327,9 +300,9 @@ void printOutput(){
 }
 
 
-void freeMemory(){
-	free(pages_per_file);
-	for(int i=0;i<total_pages;i++){
+void freeMemory() {
+	free(pagePerFile);
+	for(int i=0;i<total_pages;i++) {
 		free(out[i].data);
 		free(out[i].count);
 	}
